50a7b2f5cea56c2d229659411ed415115354bd23
AV-IO <ianguile513@gmail.com>
2019-06-16 22:51:59 -0700

    adding mostly empty file

---
+++ inputhandlers/db.go
@@ -0,0 +1,13 @@
+package inputhandlers
+
+func handleMySQL() {
+
+}
+
+// HandleDB handles the reading of database files.
+// Currently supported formats are:
+// _______ (none)
+func HandleDB(path string, results chan UPH) {
+
+	close(results)
+}
